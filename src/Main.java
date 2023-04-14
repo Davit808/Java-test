909bede0acd5a283e496ef57773f596737b8726d
Davit808 <arzumanyan.davit.92@gmail.com>
2023-04-14 12:24:45 +0400

    Add main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        A x = new B("Davit");
-        System.out.println(x.name);
-
-        String name = x.f("Ara");
-        System.out.println(name);
+        Wheel wheel1 = new Wheel();
+        wheel1.changeOfPositionForCenter(false);
+        wheel1.aroundItsAxis();
+        wheel1.speed(0);
+        wheel1.wise(Direction.LEFT);
+        Planet Earth = new Planet(75431268, 256, 6874);
+        Earth.changeOfPositionForCenter(true);
+        Earth.changeOfPositionForCenter(false);
+        Earth.speed(0);
     }
 }
